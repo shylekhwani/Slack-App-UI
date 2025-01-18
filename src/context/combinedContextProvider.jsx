136339daa1b0696800285e9baa198063da794f81
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-01-18 10:47:38 +0530

    Added Socket io for client

--- src/context/combinedContextProvider.jsx
+++ src/context/combinedContextProvider.jsx
@@ -2,12 +2,14 @@ import { combineContext } from "@/utils/combineContext";
 
 import { AuthContextProvider } from "./AuthContext/authContextProvider";
 import { CreateChannelContextProvider } from "./ChannelContext/createChannelContext";
+import { SocketContextProvider } from "./SocketContext/SocketContext";
 import { CreateWorkspaceContextProvider } from "./WorkspaceContext/CreateWorkspaceContext";
 import { WorkspaceContextProvider } from "./WorkspaceContext/WorkspaceContext";
 import { WorkspacePreferenceModalProvider } from "./WorkspacePreferenceModalContext/WorkspacePreferenceModalContext";
 
 
 export const CombinedContextProvider = combineContext(
+    SocketContextProvider,
     AuthContextProvider,
     WorkspaceContextProvider,
     CreateWorkspaceContextProvider,
