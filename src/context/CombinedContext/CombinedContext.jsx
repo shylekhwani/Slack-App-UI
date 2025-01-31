66c472cbe08b271785fd96b183a8ee61b425c242
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-01-31 17:31:15 +0530

    bug fix

--- src/context/CombinedContext/CombinedContext.jsx
+++ src/context/CombinedContext/CombinedContext.jsx
@@ -1,13 +1,13 @@
 import { mergeAllContext } from "@/utils/MergeAllContext";
 
-import { AuthContextProvider } from "./AuthContext/AuthContextProvider";
-import { CreateChannelContextProvider } from "./ChannelContext/CreateChannelContext";
-import { AddMemberContextProvider } from "./MemberContext/AddMemberContext";
-import { ChannelMessagesProvider } from "./MessagesContext/ChannelMessages";
-import { SocketContextProvider } from "./SocketContext/SocketContext";
-import { CreateWorkspaceContextProvider } from "./WorkspaceContext/CreateWorkspaceContext";
-import { WorkspaceContextProvider } from "./WorkspaceContext/WorkspaceContext";
-import { WorkspacePreferenceModalProvider } from "./WorkspacePreferenceModalContext/WorkspacePreferenceModalContext";
+import { AuthContextProvider } from "../AuthContext/AuthContextProvider";
+import { CreateChannelContextProvider } from "../ChannelContext/CreateChannelContext";
+import { AddMemberContextProvider } from "../MemberContext/AddMemberContext";
+import { ChannelMessagesProvider } from "../MessagesContext/ChannelMessages";
+import { SocketContextProvider } from "../SocketContext/SocketContext";
+import { CreateWorkspaceContextProvider } from "../WorkspaceContext/CreateWorkspaceContext";
+import { WorkspaceContextProvider } from "../WorkspaceContext/WorkspaceContext";
+import { WorkspacePreferenceModalProvider } from "../WorkspacePreferenceModalContext/WorkspacePreferenceModalContext";
 
 export const CombinedContext_Provider = mergeAllContext(
     ChannelMessagesProvider,
